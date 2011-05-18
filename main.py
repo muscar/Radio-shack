fdbefc5f54bb29a4f377e17695d6810fe0c1d5e3
Alex Muscar <muscar@gmail.com>
2011-05-18 17:41:30 +0300

    Added some operations to the WS (add, update, remove and all).

--- main.py
+++ main.py
@@ -2,18 +2,38 @@
 
 from tornadorpc import private, start_server
 
+from pymongo.objectid import ObjectId
+
 from lib.WSHandler import WSHandler
 
 class Handler(WSHandler):
-    _DB = "test"
+    _DB = "radioshack"
 
-    def add(self, msg):
-        message = { "content": msg }
-        self.db.messages.insert(message)
-        return True
+    def add(self, name, freqs=None, streams=None):
+        radio = { "name": name,
+                  "frequencies": freqs or [],
+                  "streams": streams or [] }
+        id = self.db.radios.insert(radio)
+        return str(id)
+
+    def update(self, id, name=None, freqs=None, streams=None):
+        details = { "name": name } if name else {}
+        if freqs is not None:
+            details["freqs"] = freqs
+        if streams is not None:
+            details["streams"] = streams
+        if details:
+            self.db.radios.update({ "_id": ObjectId(id) }, { "$set": details })
+            return True
+        return False
+
+    def remove(self, id):
+        return self.db.radios.remove({ "_id": ObjectId(id) })
 
     def all(self):
-        messages = [{ "content": m["content"] } for m in self.db.messages.find()]
-        return messages
+        radios = list(self.db.radios.find())
+        for r in radios:
+            r["_id"] = str(r["_id"])
+        return radios
 
-start_server(Handler, port=8000)
+start_server(Handler, route="/api/", port=8000)
